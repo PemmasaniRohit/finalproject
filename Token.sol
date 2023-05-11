d2ce4920b4b5558cdd360c172fd682978cf0eaf6
PemmasaniRohit <90728740+PemmasaniRohit@users.noreply.github.com>
2023-05-11 09:34:31 -0500

    Create Token.sol

---
+++ Token.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./IERC20.sol";
+
+/**
+ *  Exercise: [1] Write a smart contract that implements the ERC-20 standard.
+ *            [2] Create a file `src/test/Token.t.sol` and write test cases for your ERC-20 smart contract.
+ *                Look at the test case for exercise-1 as reference.
+ *                
+**/
+
+contract Token is IERC20 {
+    constructor() {}
+}
