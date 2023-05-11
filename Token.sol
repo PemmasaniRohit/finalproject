9f5f8f55107b411f469d7262aab7b6282a6fc2b9
PemmasaniRohit <90728740+PemmasaniRohit@users.noreply.github.com>
2023-05-11 09:49:10 -0500

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -11,5 +11,27 @@ import "./IERC20.sol";
 **/
 
 contract Token is IERC20 {
-    constructor() {}
+    event Bought(uint256 amount);
+    event Sold(uint256 amount);
+    IERC20 public token;
+    constructor() {
+	        token = new ExchangeERC20Program();
+	    }	
+	    function buy() payable public {
+	        uint256 selection = msg.value;
+	        uint256 balanceofUser = token.balanceOf(address(this));
+	        require(selection > 0, "You need to send some ether");
+	        require(selection <= balanceofUser, "Not enough tokens in the reserve");
+	        token.transfer(msg.sender, selection);
+	        emit Bought(selection);
+	    }
+	
+	    function sell(uint256 etherQuantity) public {
+	        require(etherQuantity > 0, "You need to sell at least some tokens");
+	        uint256 allowance = token.allowance(msg.sender, address(this));
+	        require(allowance >= etherQuantity, "Check the token allowance");
+	        token.transferFrom(msg.sender, etherQuantity(this), etherQuantity);
+	        payable(msg.sender).transfer(etherQuantity);
+	        emit Sold(etherQuantity);
+	    }
 }
